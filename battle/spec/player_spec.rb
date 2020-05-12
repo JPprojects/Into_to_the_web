9115058e3283ce4657734ad378c776d3243e3a49
Student <student@Makerss-Air-2.broadband>
2020-05-12 16:55:41 +0100

    add new class of game and split functionality

--- battle/spec/player_spec.rb
+++ battle/spec/player_spec.rb
@@ -12,18 +12,4 @@ RSpec.describe Player do
   it 'returns the hit points' do
     expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
   end
-
-
-
-  it 'damages the player' do
-    expect(mittens).to receive(:receive_damage)
-    dave.attack(mittens)
-  end
-
-
-  it 'reduces the player hit points' do
-    expect { dave.receive_damage }.to change { dave.hit_points }.by(-10)
-  end
-
-
 end
